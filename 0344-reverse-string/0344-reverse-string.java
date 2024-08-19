bbcf25735539256abb223e2a93608d7384fcbff2
S FAIZ BANU <116164987+Faizsheik@users.noreply.github.com>
2024-08-19 16:11:53 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (77.00%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s)
+    {
+        
+        for(int i = 0 ; i<=(s.length-1)/2; i++)
+        {
+            char c = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = c;
+        }
+        
+        
+    }
+}
