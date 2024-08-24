5430c409fd5ae624cab7dd69f53de9172d92ed49
S FAIZ BANU <116164987+Faizsheik@users.noreply.github.com>
2024-08-24 11:33:24 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (19.18%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -2,11 +2,42 @@ class Solution
 {
     public int lengthOfLastWord(String s) 
     {
-      String ts = s.trim();
-      String[] str = ts.split(" ");
-      String laststr = str[str.length-1];
-      return laststr.length();
-            
-            
+      
+        
+       int length = 0;
+        String str = s.trim();
+       for(int i=str.trim().length()-1;i>=0;i--)
+       {
+           if(str.charAt(i) != ' ')
+           {
+               length++;
+           }
+           else
+           {
+               break;
+           }
+       }
+        
+       return length;
+        
+        
+        
+// int length = 0;
+// for(int i=s.trim().length()-1;i>=0;i--)  //here trimmed string is not stored 
+// {
+//     if(s.charAt(i) != ' ')     //afgain using original strinh that's why error.
+//     {
+//         length++;
+//     }
+//     //return length; --> when encounters first non space character it return the length..
+//     else
+//     {
+//        if(length>0) break;
+//     }
+// }
+ 
+ 
+// return length;
     }
+                    
 }
