64f47b3ba16995bd34d22001ee067efdb9495dc9
S FAIZ BANU <116164987+Faizsheik@users.noreply.github.com>
2024-09-11 17:05:35 +0530

    Time: 18 ms (18.43%), Space: 55.6 MB (7.39%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -3,27 +3,59 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        // Step 1: Sort the array
+    public List<Integer> findDisappearedNumbers(int[] nums) 
+    {
         Arrays.sort(nums);
-        
         List<Integer> result = new ArrayList<>();
         int n = nums.length;
-        int expected = 1; // We start expecting number 1
         
-        // Step 2: Find missing numbers
-        for (int num : nums) {
-            while (num > expected) {
+       /* if(nums[0]==nums[n-1])
+        {    
+                if(nums[0] == n)
+                {
+                    int e = 1;
+                    while(e<n)
+                    {
+                        result.add(e);
+                        e++;
+                    }
+                }
+                else if(nums[0]<n)
+                {
+                    result.add(n);
+                    
+                }
+        }
+        
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(!((nums[i+1]==nums[i] + 1) || (nums[i+1] == nums[i])))
+            {
+               int  diff = nums[i+1] - nums[i];
+                int e = 1;
+                while(e<diff)
+                {
+                    result.add(nums[i]+e);
+                    e++;
+                }
+            }
+        }*/
+        
+        int expected = 1; 
+        
+        for (int num : nums) 
+        {
+            while (num > expected) 
+            {
                 result.add(expected);
                 expected++;
-            }
-            // Move to the next expected number
-            if (num == expected) {
+            } 
+            if (num == expected)
+            {
                 expected++;
             }
         }
-        
-        // Add any remaining missing numbers up to n
         while (expected <= n) {
             result.add(expected);
             expected++;
